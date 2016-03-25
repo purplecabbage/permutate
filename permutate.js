cd264b82a2a006fbd516cac4982441d6a3846263
Jesse MacFadyen <purplecabbage@gmail.com>
2016-03-25 02:42:46 -0700

    added compareFunction for user defined comparison ops, this closes #1

--- permutate.js
+++ permutate.js
@@ -1,11 +1,19 @@
 
 exports = module.exports = {
-    next:function next(arr) {
+    next:function next(arr,compareFunction) {
         var retArr = arr.slice();
+        safeCompare = compareFunction || function(a,b) {
+            if(a < b) {
+                return -1;
+            } else if(a > b) {
+                return 1;
+            }
+            return 0;
+        };
         for(var k = arr.length - 2; k > -1; k--) {
             if(arr[k] < arr[k+1]){
                 for(var l = arr.length - 1; l > k; l--) {
-                    if(arr[k] < arr[l]) {
+                    if( safeCompare(arr[k], arr[l]) < 0 ) {
                         retArr[k] = arr[l];
                         retArr[l] = arr[k];
                         var sub = retArr.slice(k+1);
@@ -18,13 +26,13 @@ exports = module.exports = {
         }
         return null;
     },
-    first:function first(arr) {
+    first:function first(arr,compareFunction) {
         var ret = arr.slice();
-        ret.sort();
+        ret.sort(compareFunction);
         return ret;
     },
-    last:function last(arr) {
-        var ret = this.first(arr);
+    last:function last(arr,compareFunction) {
+        var ret = this.first(arr,compareFunction);
         return ret.reverse();
     }
-}
+};
